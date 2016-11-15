05fe9d1e580e2c883e096a9ad2e48d59c3abc74c
latharaniel <lopezpinteno.claudia@gmail.com>
2016-11-15 18:28:50 +0100

    Éste commit es para mostrar la hora actual

--- TareaPresencialED02/src/HolaMundo.java
+++ TareaPresencialED02/src/HolaMundo.java
@@ -1,9 +1,18 @@
+import java.util.Calendar;
+import java.util.GregorianCalendar;
 
 public class HolaMundo {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		System.out.println ("Hola Mundo");
+		
+		Calendar calendario = new GregorianCalendar();
+		int hora, minutos, segundos;
+		hora =calendario.get(Calendar.HOUR_OF_DAY);
+		minutos = calendario.get(Calendar.MINUTE);
+		segundos = calendario.get(Calendar.SECOND);
+		System.out.println(hora + ":" + minutos + ":" + segundos);
 	}
 
 }
